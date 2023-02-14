fea480dd9cfbd29ccd716b123773975374955d44
KeitaroKawahara <keitaro.inc@gmail.com>
2023-02-15 07:59:25 +0900

    sheetP resentationControllerでシートサイズを指定できるようにした

--- HalfModalTest/HalfModalViewController.swift
+++ HalfModalTest/HalfModalViewController.swift
@@ -7,13 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HalfModalViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
     }
-
-
+    
+    //MARK: sheetPresentationControllerを使用しないやり方
+    /*
+     let modalHeight: CGFloat = 300 // モーダルの高さ
+     
+     override func viewDidLoad() {
+     super.viewDidLoad()
+     
+     // モーダルの背景Viewを作成
+     let backgroundView = UIView(frame: view.bounds)
+     backgroundView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+     view.addSubview(backgroundView)
+     
+     // モーダルのコンテンツViewを作成
+     let modalView = UIView(frame: CGRect(x: 0, y: view.bounds.height - modalHeight, width: view.bounds.width, height: modalHeight))
+     modalView.backgroundColor = UIColor.white
+     view.addSubview(modalView)
+     
+     // モーダルの表示アニメーションを作成
+     UIView.animate(withDuration: 0.3) {
+     modalView.frame.origin.y = self.view.bounds.height - self.modalHeight
+     }
+     
+     // モーダルの背景Viewをタップしたときのアクションを作成
+     let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissModal))
+     backgroundView.addGestureRecognizer(tapGesture)
+     }
+     
+     @objc func dismissModal() {
+     // モーダルを非表示にするアニメーションを作成
+     UIView.animate(withDuration: 0.3, animations: {
+     self.view.subviews.last?.frame.origin.y = self.view.bounds.height
+     }) { (_) in
+     self.dismiss(animated: false, completion: nil)
+     }
+     }
+     }
+     
+     */
 }
-
--- HalfModalTest/MainViewController.swift
+++ HalfModalTest/MainViewController.swift
@@ -5,4 +5,38 @@
 //  Created by Keitaro Kawahara on 2023/02/15.
 //
 
-import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let button = UIButton(type: .system)
+        button.setTitle("Show Modal", for: .normal)
+        button.addTarget(self, action: #selector(showModal), for: .touchUpInside)
+        view.addSubview(button)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    @objc func showModal() {
+        let modalVC = HalfModalViewController()
+        modalVC.modalPresentationStyle = .pageSheet
+        modalVC.preferredContentSize = CGSize(width: view.bounds.width, height: view.bounds.height * 0.5)
+        present(modalVC, animated: true, completion: nil)
+        
+        if let sheet = modalVC.sheetPresentationController {
+            sheet.detents = [.medium()]
+            sheet.largestUndimmedDetentIdentifier = .medium
+        }
+    }
+}
+
+
+
+
+
